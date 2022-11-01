9e25bf2e142efc49515340db6e480d5e0e14786c
ElieSun <elinavoloshyk@gmail.com>
2022-11-01 15:04:24 +0200

    task-01

---
+++ src 2 /js/01-gallery.js
@@ -0,0 +1,40 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+console.log(galleryItems);
+const galleryListRefs = document.querySelector(".gallery");
+const markup = galleryItems
+  .map((img) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${img.original}">
+        <img
+        class="gallery__image"
+        src="${img.preview}"
+        data-source="${img.original}"
+        alt="${img.description}"
+        />
+    </a>
+            </div>`;
+  })
+  .join("");
+galleryListRefs.innerHTML = markup;
+
+galleryListRefs.addEventListener("click", handleOpenModale);
+function handleOpenModale(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`,
+    { onShow: (instance) => {window.addEventListener('keydown', closeModal)}, 
+    onClose: (instance) => {window.removeEventListener('keydown', closeModal)} }
+  );
+  function closeModal(event) {
+    if (event.code === 'Escape') {
+        instance.close();
+    }
+  }
+  instance.show();
+}
