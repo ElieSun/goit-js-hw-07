037cb794c2fc73f2ed9d42c3624f709074a9db6f
ElieSun <elinavoloshyk@gmail.com>
2022-11-08 13:30:31 +0200

    add_options

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,7 +1,4 @@
 import { galleryItems } from "./gallery-items.js";
-// import SimpleLightbox from "simplelightbox";
-// Change code below this line
-// console.log(galleryItems);
 const galleryListRefs = document.querySelector(".gallery");
 const markup = galleryItems.map((img) => {
     return `<a class="gallery__item" href="${img.original}">
@@ -9,35 +6,8 @@ const markup = galleryItems.map((img) => {
   </a>`;
   }).join("");
 galleryListRefs.innerHTML = markup;
+new SimpleLightbox('.gallery a', { animationSpeed: 250 });
 
-// galleryListRefs.addEventListener("click", handleOpenModale);
 
-let simpleLightbox = new SimpleLightbox('.gallery a', {});
-
-// function handleOpenModale(event) {
-  // event.preventDefault();
-  // if (!event.target.classList.contains("gallery__image")) {
-  //   return;
-  // }
-  // simpleLightbox.on('show.simplelightbox', function () {
-  //     // Do something…
-  // });
-
-  // const instance = simpleLightbox.create(
-  //   `<img src="${event.target.dataset.source}" width="800" height="600">`,
-  //   { 
-  //     onShow: (instance) => {window.addEventListener('keydown', closeModal)}, 
-  //     onClose: (instance) => {window.removeEventListener('keydown', closeModal)} 
-  // }
-  // );
-  // function closeModal(event) {
-  //   if (event.code === 'Escape') {
-  //       instance.close();
-  //   }
-  // }
-  // instance.show();
-// }
-// console.log(SimpleLightbox);
-// new SimpleLightbox('.gallery a', { /* options */ });
 
 
