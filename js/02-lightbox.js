5a03c192f8a1217d593e9f4e7b165adfb169a9bc
ElieSun <elinavoloshyk@gmail.com>
2022-11-10 12:13:29 +0200

    add_options

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,11 +2,11 @@ import { galleryItems } from "./gallery-items.js";
 const galleryListRefs = document.querySelector(".gallery");
 const markup = galleryItems.map((img) => {
     return `<a class="gallery__item" href="${img.original}">
-    <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+    <img class="gallery__image" src="${img.preview}" alt="${img.description}"/>
   </a>`;
   }).join("");
 galleryListRefs.innerHTML = markup;
-new SimpleLightbox('.gallery a', { animationSpeed: 250 });
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
 
 
 
